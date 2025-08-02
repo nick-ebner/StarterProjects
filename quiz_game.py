500c637a472ba86471a0bff4e44e96018b0f3b9b
Nick <nicholas.ebner9@gmail.com>
2025-08-01 20:47:41 -0400

    Added a welcome message and difficulty selection to the quiz game.

--- quiz_game.py
+++ quiz_game.py
@@ -1,25 +1,28 @@
 import random;
 
-top_of_range = int(input("Type a number: "))
-
-target = random.randint(0,top_of_range)
-guesses = 5
-
+print("-----------Welcome to the Number Guessing Game!-----------") 
+print("I'm thinking of a number between 0 and 25. What level of \ndifficulty would you like to play with?\n\t1 - Easy (10 guesses)\n\t2 - Medium (7 guesses)\n\t3 - Hard (5 guesses)")
+selection = int(input("\nEnter a number: "))
+if selection == 1:
+    guesses = 10
+elif selection == 2:
+    guesses = 7
+elif selection == 3:
+    guesses = 5
+target = random.randint(0,25)
 while True:
-
-    print(f"You have {guesses} guesses")
+    print(f"\nYou have {guesses} guesses")
     guess = int(input("Type a number: "))
-
     if guess == target:
-        print(f"Congratulations! {guess} is the correct answer")
+        print(f"---------Congratulations! {target} is the correct answer---------")
         quit()
-    
-    if guess > target:
-        print("That guess is too high. Please try again!")
-    else:
-        print("That guess is too low. Please try again!")
-    
+
     guesses = guesses-1
-    if guesses == 0:
-        print(f"You lose! The correct answer was {target}!")
+    if guess > 0:
+        if guess > target:
+            print("That guess is too high. Please try again!")
+        else:
+            print("That guess is too low. Please try again!")
+    else:
+        print(f"------------You lose! The correct answer was {target}------------") 
         break
